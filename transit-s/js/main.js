a6818bd25dbd8c7ecbf67a15ca0142e9834efa3a
khasenovweb <hasnovkirill93@gmail.com>
2020-02-04 08:29:07 +0500

    dkjdkfjdfkj

--- transit-s/js/main.js
+++ transit-s/js/main.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-	$.fias.token = '6NDrn5bD8aZR84y3Hb8Hfb3HiDGzGaSS';
-	$.fias.url = 'https://kladr-api.com/api.php';
-	$('.input').fias({
-		oneString: true
-	});
+	
 });
