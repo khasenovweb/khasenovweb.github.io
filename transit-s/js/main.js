42ee7be0e144dbc12a266c22ec0e64300494df5e
khasenovweb <hasenovkirill93@gmail.com>
2020-02-04 16:30:09 +0500

    fkdjfkdfj

--- transit-s/js/main.js
+++ transit-s/js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-	
+	$('[data-click-calc]').click(function(){
+		$('.calc').addClass('calc__show');
+		$(this).addClass('nav__calc__active');
+	});
+
+	$('body').click(function(){
+		if ($(event.target).closest(".calc").length) return;
+		if ($(event.target).closest("[data-click-calc]").length) return;
+		$("[data-click-calc]").removeClass('nav__calc__active');
+		$('.calc').removeClass('calc__show');
+	});
+
+	$('[data-click-modal__wrapper]').click(function(){
+		$('.modal__wrapper').show();
+	});
+	$('.modal__wrapper').click(function(){
+		if ($(event.target).closest(".modal").length) return;
+		$('.modal__wrapper').hide();
+	});
 });
