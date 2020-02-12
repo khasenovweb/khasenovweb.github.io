c1e4cc5215d2dcf7586f02c70722e0bd28958f52
khasenovweb <hasenovkirill93@gmail.com>
2020-02-12 23:46:56 +0500

    dfdfkj

---
+++ vtishine/js/mapbasics.js
@@ -0,0 +1,23 @@
+var myMap;
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('objects__map', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [55.76, 37.64], // Москва
+        zoom: 7
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    document.getElementById('destroyButton').onclick = function () {
+        // Для уничтожения используется метод destroy.
+        myMap.destroy();
+    };
+
+}
