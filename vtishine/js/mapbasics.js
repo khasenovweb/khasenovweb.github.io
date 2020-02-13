b964a49bdef7535504f601617570fc2552c542c1
khasenovweb <hasenovkirill93@gmail.com>
2020-02-13 09:53:03 +0500

    kjsdh

--- vtishine/js/mapbasics.js
+++ vtishine/js/mapbasics.js
@@ -1,23 +1,50 @@
-var myMap;
-
-// Дождёмся загрузки API и готовности DOM.
 ymaps.ready(init);
 
-function init () {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
-    myMap = new ymaps.Map('objects__map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [55.76, 37.64], // Москва
-        zoom: 7
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [55.159897, 61.402554],
+            zoom: 10
+        });
 
-    document.getElementById('destroyButton').onclick = function () {
-        // Для уничтожения используется метод destroy.
-        myMap.destroy();
-    };
+        var myPlacemark_1 = new ymaps.Placemark([ 55.152557, 61.411573],{
+                  balloonContentHeader: "Название объекта",
+                  balloonContentBody: 'Описание объекта',
+                  balloonContentFooter: "",
+                  hintContent: "Кликните для более подробной информации"
+              },
+              {
+                  iconLayout: 'default#image',
+                  iconImageHref: 'img/map__marker__red.png',
+                  iconImageSize: [110, 116],
+                  //iconImageOffset: [0, -80]
+              });
+        myMap.geoObjects.add(myPlacemark_1);
 
+        var myPlacemark_2 = new ymaps.Placemark([ 55.190678, 61.311626],{
+                  balloonContentHeader: "Название объекта",
+                  balloonContentBody: 'Описание объекта',
+                  balloonContentFooter: "",
+                  hintContent: "Кликните для более подробной информации"
+              },
+              {
+                  iconLayout: 'default#image',
+                  iconImageHref: 'img/map__marker__blue.png',
+                  iconImageSize: [110, 116],
+                  //iconImageOffset: [0, -80]
+              });
+        myMap.geoObjects.add(myPlacemark_2);
+
+        var myPlacemark_3 = new ymaps.Placemark([ 55.116943, 61.461995],{
+                  balloonContentHeader: "Название объекта",
+                  balloonContentBody: 'Описание объекта',
+                  balloonContentFooter: "",
+                  hintContent: "Кликните для более подробной информации"
+              },
+              {
+                  iconLayout: 'default#image',
+                  iconImageHref: 'img/map__marker__yellow.png',
+                  iconImageSize: [110, 116],
+                  //iconImageOffset: [0, -80]
+              });
+        myMap.geoObjects.add(myPlacemark_3);
 }
